48e4e9242ca987cdd5052a93dd93449a5f2c5744
Marcus Pereira de Souza <marcus.souza72@gmail.com>
2026-07-23 13:53:43 -0300

    Registra models no pacote

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,1 +1,11 @@
-# Arquivo temporariamente vazio
+from app.models.ml_account import MLAccount
+from app.models.ml_item import MLItem
+from app.models.oauth_state import OAuthState
+from app.models.user import User
+
+__all__ = [
+    "User",
+    "MLAccount",
+    "MLItem",
+    "OAuthState",
+]
